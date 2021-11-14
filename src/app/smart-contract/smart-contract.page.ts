42da7f344fb1d233b1f79e15ed39165af60e378e
Garry Kabongo <garry.kabongo@true.nl>
2021-11-14 05:27:21 +0200

    made a number of ui changes. things look more presentable now.

--- src/app/smart-contract/smart-contract.page.ts
+++ src/app/smart-contract/smart-contract.page.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SmartContractPage implements OnInit {
 
+  private buttonColor: string = "primary";
+  private buttonColor2: string = "primary";
+  private buttonColor3: string = "primary";
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleFirstColor() {
+    if(this.buttonColor === '#FF6E05') {
+      this.buttonColor = '#FFFFFF';
+    } else {
+      this.buttonColor = '#FF6E05';
+    }
+  }
+  toggleSecondColor() {
+    if(this.buttonColor2 === '#FF6E05') {
+      this.buttonColor2 = '#FFFFFF';
+    } else {
+      this.buttonColor2 = '#FF6E05';
+    }
+  }
+  toggleThirdColor() {
+    if(this.buttonColor3 === '#FF6E05') {
+      this.buttonColor3 = '#FFFFFF';
+    } else {
+      this.buttonColor3 = '#FF6E05';
+    }
   }
 
 }
